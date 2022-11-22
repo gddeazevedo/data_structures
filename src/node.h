4fb3badfe6919dbca18427ee39cec075e08cfd64
gddeazevedo <gabrielcapuerj@gmail.com>
2022-11-22 16:07:16 -0300

    adding double node

--- src/node.h
+++ src/node.h
@@ -6,4 +6,10 @@ typedef struct __node {
     struct __node* next;
 } Node;
 
+typedef struct __double_node {
+    int data;
+    struct __double_node* prev;
+    struct __double_node* next;
+} DoubleNode;
+
 #endif
